992dd58c7c78e4bca8c29200db0d3df8c8ae8124
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-10 13:32:36 -0600

    uses genus, not OTU

--- code/R/bias_correct.R
+++ code/R/bias_correct.R
@@ -3,16 +3,13 @@ get_bc_abund <- function(fname) {
   
   output <- readRDS(fname)
   
-  bc_out <- output$bias_correct_log_table
-  
-  taxonomy <- get_taxonomy(ps_ASV)
   metadata <- get_metadata(ps_ASV)
   
-  bc_long <- bc_out %>%
-    rownames_to_column("OTU") %>%
-    pivot_longer(-OTU, names_to = "Sample", values_to = "bc_abund") %>%
-    inner_join(taxonomy, by = "OTU") %>%
-    inner_join(metadata, by = "Sample") %>%
+  bc_long <- output$bias_correct_log_table %>%
+    rownames_to_column("Genus") %>%
+    filter(!is.na(Genus)) %>%
+    pivot_longer(-Genus, names_to = "Sample", values_to = "bc_abund") %>%
+    left_join(metadata, by = "Sample") %>%
     dplyr::select(Sample, size.mm, size.name, Genus, bc_abund) 
   
   return(bc_long)
