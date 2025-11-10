ea186d86be1c6ebb2c16cea19d016d8a543fc057
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-10 09:41:51 -0600

    Added seed. Made output column contain Genus.

--- code/R/single-use/run_aldex2_t_test.R
+++ code/R/single-use/run_aldex2_t_test.R
@@ -1,15 +1,27 @@
 rm(list = ls())
 
+library(tidyverse)
+library(digest)
+library(phyloseq)
 library(ALDEx2)
 
 size1 <- c("M", "L", "XL", "XXL")
 size2 <- c("S", "S", "S", "S")     # reference group
 
-ps <- readRDS("./data/ps_genus.rds") 
-size_name <- levels(ps@sam_data$size.name)
+ps_ASV <- readRDS("./data/ps_ASV.rds") 
 
-metadata <- as.data.frame(as.matrix(ps@sam_data)) %>%
+# Remove taxa not seen more than 3 times (reads) in at least 20% of the samples.
+# This protects against an OTU with small mean & trivially large C.V.
+ps_filt = filter_taxa(ps_ASV, function(x) sum(x > 3) >= (0.2*length(x)), TRUE)
+
+# Agglomerate by Genus
+ps_genus <- tax_glom(ps_filt, taxrank = "Genus")
+
+metadata <- as.data.frame(as.matrix(ps_genus@sam_data)) %>%
   rownames_to_column("Sample") 
+
+taxonomy <- as.data.frame(as.matrix(ps_genus@tax_table)) %>%
+  rownames_to_column("OTU")
 
 # Generate pairwise combinations
 pair_samples <- cbind(size1, size2) %>%
@@ -27,7 +39,10 @@ pair_samples <- cbind(size1, size2) %>%
       # Get conditions
       conds <- metadata$size.name[match(samples_in_pair, metadata$Sample)]
     }),
-    res = map(samples, function(samples_in_pair) {
+    res = map2(samples, comparison, function(samples_in_pair, comp_name) {
+      # Set a unique seed for each comparison name
+      seed_val <- as.integer(substr(digest::digest(comp_name), 1, 8), 16)
+      set.seed(seed_val)
       # Subset counts table
       reads <- as.data.frame(ps@otu_table) %>%
         dplyr::select(all_of(samples_in_pair))
