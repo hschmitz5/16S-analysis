f47421ce7443524b16aaa4303200367fa2c8cb90
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-10 09:41:11 -0600

    Removed filtering and agglomeration. Saving only ASV data now.

--- code/R/single-use/define_ps.R
+++ code/R/single-use/define_ps.R
@@ -34,16 +34,8 @@ ps_filt0 <- phyloseq::subset_taxa(ps, Kingdom != "Unassigned")
 # define minimum depth to rarefy
 rarefy_level <- min(sample_sums(ps_filt0))  # lowest number of ASVs per sample
 # apply rarefaction
-ps_filt_r <-rarefy_even_depth(
+ps_filt <-rarefy_even_depth(
   ps_filt0, rarefy_level, rngseed = 7, replace = TRUE, trimOTUs = TRUE, verbose = TRUE
 )
   
-# Remove taxa not seen more than 3 times (reads) in at least 20% of the samples. 
-# This protects against an OTU with small mean & trivially large C.V.
-ps_filt = filter_taxa(ps_filt_r, function(x) sum(x > 3) >= (0.2*length(x)), TRUE)
-
 saveRDS(ps_filt, file = "./data/ps_ASV.rds")
-
-ps_genus <- tax_glom(ps_filt, taxrank = "Genus")
-
-saveRDS(ps_genus, file = "./data/ps_genus.rds")
