5ec1219f3c44dc6298138d2f2aee9ac40e201c48
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-13 11:36:40 -0600

    split bars by sample

--- code/R/plot_rel_stacked_bar.R
+++ code/R/plot_rel_stacked_bar.R
@@ -1,44 +1,46 @@
+source("./code/R/00_setup.R")
+source("./code/R/01_load_data.R")
+source("./code/R/02_process_ps.R")
+
 # display top n most abundant genera
 n_display <- 10
 
 fname <- "./figures/abund_size_stacked.png"
-  
-# Average per Genus across replicates
+
 genus_size <- get_rel_genus(ps) %>%
-  mutate(Genus = as.character(Genus)) %>%  # ensure it's character
-  group_by(Genus, size.mm, size.name) %>%
-  summarise(
-    mean_ab = mean(Abundance),
-    sd_ab = sd(Abundance),
-    .groups = "drop") %>%
+  dplyr::select(Sample, size.mm, size.name, Genus, Abundance) %>%
+  # Rename genera outside top n_display as "Other"
   mutate(
     Genus = ifelse(is.na(Genus) | !(Genus %in% high_ab_genera[1:n_display]), "Other", Genus)
   ) %>%
-  group_by(Genus, size.mm, size.name) %>%
+  group_by(Sample, size.mm, size.name, Genus) %>%
   summarise(
-    mean_ab = sum(mean_ab),
-    sd_ab = ifelse(unique(Genus) == "Other", NA_real_, sd_ab),
+    Abundance = sum(Abundance),
     .groups = "drop"
   ) %>%
   mutate(Genus = factor(Genus, levels = c(high_ab_genera[1:n_display], "Other")))
   
 #n_display <- length(unique(genus_size$Genus)) - 1
 
-p <- ggplot(genus_size, aes(x = size.name, y = mean_ab, fill = fct_rev(Genus))) +
-  geom_col(width = 0.7) +  # stacked by default
+p <- ggplot(genus_size, aes(x = Sample, y = Abundance, fill = fct_rev(Genus))) +
+  # group samples by size
+  facet_grid(. ~ size.name, scales = "free_x", space = "free_x", switch = "x") +  
+  geom_col(width = 0.95) + # space between same size
   scale_fill_manual(
     values = c("gray", met.brewer(taxa_pal, n_display)),
     name = "Genera"
   ) +
-  scale_y_continuous(
-    expand = expansion(mult = c(0, 0.1))  # Add 10% space above
-  ) +
   labs(
-    # title = paste0("Genera = ", n_display),
     x = "Size",
-    y = "Mean Relative Abundance [%]"
+    y = "Relative Abundance [%]"
   ) +
-  theme_minimal(base_size = 16)
+  theme_minimal(base_size = 10) +
+  theme(
+    axis.text.x = element_blank(),    # hide sample labels
+    axis.ticks.x = element_blank(),
+    strip.placement = "outside",      # place strips below the panel
+    strip.text.x = element_text(size = 10, margin = margin(t = 5))
+  )
 
 # Save plot
-ggsave(fname, plot = p, width = 8, height = 4, dpi = 300)
+ggsave(fname, plot = p, width = 6, height = 4, dpi = 300)
