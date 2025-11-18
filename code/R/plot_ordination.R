ebfd23ba6517cda1759863ef30506f1a61222853
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-18 10:42:14 -0600

    Added comparison to all samples

--- code/R/plot_ordination.R
+++ code/R/plot_ordination.R
@@ -1,31 +1,48 @@
+rm(list = ls())
+library(cowplot)
 source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 source("./code/R/02_process_ps.R")
 
 fname_ord <- "./figures/ordination-PCoA.png"
 
-ps.ord1 <- ordinate(ps_ASV, "PCoA", "wunifrac")
-ps.ord2 <- ordinate(ps_ASV, "PCoA", "unifrac")
-# Alternative
-# ps.ord <- ordinate(ps_filt, "NMDS", "bray")
+# load phyloseq object for all sample sizes
+ps_full <- readRDS("./data/ps_ASV_full.rds")
 
-cols <- met.brewer(size_pal, n_sizes) 
+# ps: subset
+ps.ord1 <- ordinate(ps, "PCoA", "wunifrac")
+# ps_full: all sample groups
+ps.ord2 <- ordinate(ps_full, "PCoA", "wunifrac")
 
-p1 <- plot_ordination(ps_ASV, ps.ord1, type="samples", color="size.name") + 
-  scale_color_manual(values=cols) + 
-  labs(
-    title="PCoA (wunifrac)",
-    color = "Size") 
+cols <- met.brewer(size_pal, n_sizes)
 
-p2 <- plot_ordination(ps_ASV, ps.ord2, type="samples", color="size.name") + 
-  scale_color_manual(values=cols) + 
-  labs(
-    title="PCoA (unifrac)",
-    color = "Size") 
+p1 <- plot_ordination(ps, ps.ord1, type="samples", color="size.name") +
+  scale_color_manual(values = cols) +
+  labs(title="Granular biomass", color = "Size") +
+  theme_minimal(base_size = 12) +
+  guides(color = "none")
 
-ordination_plot <- p1 + p2 +
-  plot_layout(guides = "collect") & 
-  theme_minimal(base_size = 12) +  # ensures base_size is applied
-  theme(legend.position = "bottom")
+p2 <- plot_ordination(ps_full, ps.ord2, type="samples", color="size.name") +
+  scale_color_manual(values = c("gray", cols)) +
+  labs(title="Flocs & granules", color = "Size") +
+  theme_minimal(base_size = 12) +
+  theme(legend.position = "bottom") +
+  guides(color = guide_legend(nrow = 1))
+
+# Extract the legend
+leg <- get_legend(p2)
+
+# Remove legend from p2
+p2_clean <- p2 + theme(legend.position = "none")
+
+ordination_plot <- (p1 | p2_clean) / 
+  patchwork::wrap_elements(full = leg) +   # add legend 
+  plot_layout(heights = c(10, 1)) +        # allocate space: 10 units for plots, 1 unit for legend
+  plot_annotation(
+    title = "PCoA (wunifrac)"
+  ) &
+  theme(
+    plot.title = element_text(hjust = 0.5)  # center the title
+  )
 
 ggsave(fname_ord, plot = ordination_plot, width = 8, height = 3, dpi = 300)
