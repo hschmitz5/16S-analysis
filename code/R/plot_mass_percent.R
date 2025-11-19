27e03e608c4155b086683a9762581c8bf5543ffa
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2025-11-19 10:13:06 -0600

    added ggpattern

--- code/R/plot_mass_percent.R
+++ code/R/plot_mass_percent.R
@@ -1,3 +1,4 @@
+library(ggpattern)
 source("./code/R/00_setup.R")
 source("./code/R/01_load_data.R")
 
@@ -8,40 +9,55 @@ x0 <- c(0, 0.21, 0.43, 0.60, 1.4, 2.0, 2.8, 4.0, 5.0)   # sieve boundaries
 labels <- c("< 0.21", "0.21 - 0.43", "0.43 - 0.60", 
             "0.60 - 1.4", "1.4 - 2.0", "2.0 - 2.8", 
             "2.8 - 4.0", "> 4.0")
-y <- c(27.29, 19.05, 5.05, 11.88, 7.59, 9.85, 10.62, 8.67)
+mass_percent <- c(27.29, 19.05, 5.05, 11.88, 7.59, 9.85, 10.62, 8.67)
 
-colors <- c("black", "gray", "#444444", met.brewer(size_pal, n_sizes))
+colors <- c("black", "white", "gray", met.brewer(size_pal, n_sizes))
 
-# Compute widths and midpoints -------------------------------------
-
-w <- diff(x0)  # bar widths
-mid <- x0[-length(x0)] + w/2  # midpoint x-coordinates
+patterns <- c("none", "stripe", "none", "none", "none", "none", "none", "none")
 
 df <- data.frame(
-  mid = mid,
-  width = w,
-  percent = y,
-  label = labels,
-  color = colors
+  xmin = x0[-length(x0)],  # removes last element
+  xmax = x0[-1],           # removes first element 
+  ymin = 0,
+  ymax = mass_percent,
+  label = factor(labels, levels = labels),
+  color = colors,
+  pattern = patterns
 )
-
-df$label <- factor(df$label, levels = labels)
 
 # Plot --------------------------------------------------------------
 
-p <- ggplot(df, aes(x = mid, y = percent, fill = label, width = width)) +
-  geom_bar(stat = "identity", alpha = 0.8) +
-  scale_fill_manual(values = colors, name = "") +
-  scale_x_continuous(breaks = x0[-length(x0)], labels = x0[-length(x0)]) +
-  labs(x = "Granule Diameter [mm]",
-       y = "Mass Percentage [%]") +
+p <- ggplot(df) +
+  geom_rect_pattern(
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = label, pattern = label),
+    color = "black", linewidth = 0.2,  # outline
+    pattern_fill = "black", 
+    pattern_angle = 45,
+    pattern_density = 0.1,
+    pattern_spacing = 0.04,
+    pattern_key_scale_factor = 0.5
+  ) +
+  scale_fill_manual(values = colors) +
+  scale_pattern_manual(values = patterns) +
+  # control x-axis tick labels
+  scale_x_continuous(
+    breaks = x0[-length(x0)],
+    labels = x0[-length(x0)]
+  ) +
+  labs(
+    x = "Granule Diameter [mm]",
+    y = "Mass Percentage [%]",
+    fill = "",
+    pattern = ""
+  ) +
   theme_minimal(base_size = 14) +
   theme(
     legend.position = "top",
-    axis.text.x = element_text(angle = 45, hjust = 1)
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.grid.minor.x = element_blank()
   ) +
   guides(
-    fill = guide_legend(byrow = TRUE)   
+    fill = guide_legend(byrow = TRUE)
   )
 
 # Save figure -------------------------------------------------------
